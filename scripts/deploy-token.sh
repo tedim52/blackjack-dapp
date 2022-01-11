493456ea957aa773658dc497a5586a0ac128f389
Tedi Miitku <tbm42@cornell.edu>
2022-01-11 12:34:30 -0500

    Add script to deploy ChipToken

--- scripts/deploy-token.sh
+++ scripts/deploy-token.sh
@@ -1,1 +1,4 @@
-# Deploys a ChipToken used for casino games
+ETH_RPC_URL=https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}
+
+token=$(ETH_FROM=$ETH_FROM dapp create ChipToken -- --gas 6000000 --rpc-url $ETH_RPC_URL)
+echo "New ChipToken deployed at: $token"
